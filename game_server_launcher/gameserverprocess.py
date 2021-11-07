58feb226eb93bcb7d73c5a5ed5e7471f91f2af42
chickenbellyfin <chickenbellyfinn@gmail.com>
2021-11-07 01:08:25 -0800

    Use wineconsole instead of wine

--- game_server_launcher/gameserverprocess.py
+++ game_server_launcher/gameserverprocess.py
@@ -52,7 +52,7 @@ class GameServerProcess():
 class WineGameServerProcess():
   """
   Manage a game server process running in linux using Wine (https://www.winehq.org/)
-  wine must be installed in the environment along with vcrun2017 from winetricks
+  wine (32-bit) must be installed in the environment along with vcrun2017 from winetricks
   """
 
   def __init__(self, server, working_dir, abslog, port, control_port, dll_config_path=None, wait_time_secs=5):
@@ -63,33 +63,45 @@ class WineGameServerProcess():
     self.dll_config_path = dll_config_path
     self.server = server[-1:] # 1 or 2
     self.wait_time_secs = int(wait_time_secs)
+    self.tribes_exe = os.path.join(self.working_dir, f'TribesAscend{self.server}.exe')
 
   def start(self):
-    exe_path = os.path.join(self.working_dir, f'TribesAscend{self.server}.exe')
-    args = ['wine', exe_path, 'server',
+    # Starting with wineconsole instead of wine saves > 300MB of memory
+    args = ['wineconsole', self.tribes_exe, 'server',
       f'-abslog={self.abslog}',
       f'-port={self.port}',
       f'-controlport', str(self.control_port),
       '-noportoffset'
     ]
-    print(f"RUN {args}")
 
     if self.dll_config_path is not None:
       args.extend(['-tamodsconfig', self.dll_config_path])
 
+    print(f"Starting game server with command: {args}")
     self.process = sp.Popen(args, cwd=self.working_dir)
     
+
+    self.pid = None # PID is the wine PID. We need to use this for DLL injection
+    self.upid = None  # UPID is unix PID, which we need to use for freeze/unfreeze
+    
     # sometimes the wine process does not get shown immediately
-    self.pid = None
+    # poll until we see it
     start_time = time.time()
     while time.time() < start_time + self.wait_time_secs:
-      self.pid = self._find_tribes_windows_pid()
-      if self.pid is not None:
+      if self.pid is None:
+        self.pid = self._find_tribes_windows_pid()
+      if self.upid is None:
+        self.upid = self._find_tribes_linux_pid()
+      if self.upid and self.upid:
         break
+
       time.sleep(1)
 
     if self.pid is None:
-      raise FatalError(f'Failed to start game server process {args}')
+      raise FatalError(f'Failed to find wpid of game server process: {args}')
+
+    if self.upid is None:
+      raise FatalError(f'Failed to find upid of game server process: {args}')
   
   def poll(self):
     return self.process.poll()
@@ -101,17 +113,36 @@ class WineGameServerProcess():
     self.process.terminate()
 
   def freeze(self):
-    self.process.send_signal(signal.SIGSTOP)
+    os.kill(self.upid, signal.SIGSTOP)
     return True
 
   def unfreeze(self):
-    self.process.send_signal(signal.SIGCONT)
+    os.kill(self.upid, signal.SIGCONT)
     return True
+
+  def _find_tribes_linux_pid(self):
+    """
+    Finds the unix process id (upid) of TribesAscend.exe. Because we launched with wineconsole,
+    the actual game process (TribesAscend.exe) will be a child of it, so we need to use its pid 
+    instead of wineconsole's to freeze/unfreeze the game server.
+    https://man7.org/linux/man-pages/man5/proc.5.html
+    """
+    for pid in os.listdir('/proc'):
+      if pid.isdigit():
+        try:
+          with open(os.path.join('/proc', pid, 'cmdline')) as cmd:
+            if cmd.read().startswith(self.tribes_exe):
+              print(f"Found {self.tribes_exe} in /proc/{pid}")
+              return int(pid)
+        except Exception as e:
+         pass
+    print(f"Did not find {self.tribes_exe} in /proc")
+    return None
 
   def _find_tribes_windows_pid(self):
     """
     Finds the windows process id (wpid) of TribesAscend.exe running in wine. The matching process's
-    command must also match the port since there will be at least two TribesAscend.exe's running.
+    command must also match the server id since there will be at least two TribesAscend.exe's running.
     InjectorStandalone.exe must be supplied with the wpid, not the linux pid (upid) to find the
     process when running under wine.
     https://wiki.winehq.org/Wine_Developer%27s_Guide/Debugging_Wine#Processes_and_threads:_in_underlying_OS_and_in_Windows
