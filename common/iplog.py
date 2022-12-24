1b7c50552d8e179303b514c77c5fe0c95c4f9b73
chickenbellyfin <chickenbellyfinn@gmail.com>
2022-12-24 15:50:21 -0800

    iplog

---
+++ common/iplog.py
@@ -0,0 +1,14 @@
+
+import os
+import time
+
+IPLOG_LABEL = os.getenv('IPLOG_LABEL', 'taserver')
+IPLOG_FILE = os.getenv('IPLOG_FILE', 'iplog.tsv')
+
+
+def add(ip: str, user_id: int, display_name: str):
+  try:
+    with open(IPLOG_FILE, 'a') as f:
+      f.write(f"{IPLOG_LABEL}\t{int(time.time())}\t{user_id}\t{display_name}\t{ip}\n")
+  except Exception as e:
+    print(f"Failed to log IP ip:{ip}/user:{user_id} ({display_name}) {e}")
